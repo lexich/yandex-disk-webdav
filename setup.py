44bdbd02d679df59ceef4269927cbdd573093077
Efremov Alexey <lexich121@gmail.com>
2013-04-22 15:21:11 +0400

    fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yandex-disk-webdav',
-    version='0.0.1',
+    version='0.0.2',
     include_package_data=True,
     packages=find_packages(),
     url='https://github.com/lexich/yandex-disk-webdav',
@@ -11,5 +11,5 @@ setup(
     author='lexich',
     author_email='lexich121@gmail.com',
     description='Simple wrapper to work with yandex disk using webdav Basic Auth',
-    long_description = open("README.md","r").read()
+    long_description = "Simple wrapper to work with yandex disk using webdav Basic Auth"
 )
