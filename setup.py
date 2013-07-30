ff94a3eb364222a95cf2681723ea39505c1c71d8
Efremov Alexey <lexich121@gmail.com>
2013-07-30 16:06:05 +0400

    update config 0.2.9

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 #-*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 import os
+import sys
+
+PY3 = sys.version_info[0] == 3
+
+if PY3:
+  install_requires = ["six"]
+else
+  install_requires = ["six", "simplejson"]
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+
 setup(
     name='yandexwebdav',
-    version='0.2.8',
+    version='0.2.9',
     include_package_data=True,
     py_modules=['yandexwebdav'],
     url='https://github.com/lexich/yandex-disk-webdav',
@@ -13,9 +23,7 @@ setup(
     author_email='lexich121@gmail.com',
     description='Simple wrapper to work with yandex disk using webdav Basic Auth',
     long_description=README,
-    install_requires=[
-      "simplejson","six"
-    ],
+    install_requires=install_requires,
     scripts=[
         "ydw.py"
     ]
